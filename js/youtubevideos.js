2d8d497550364147c3b800fbe2141d099e5377ed
Ahmad Rabiu <rabiu2993@gmail.com>
2023-02-23 09:48:24 -1200

    More functionalties

--- js/youtubevideos.js
+++ js/youtubevideos.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
-
    var API_KEY = "AIzaSyDDBXvXZlraYJmPP4OKq8XLwZxYNynE-3I"
+
+   var video = ""
+
+   var videos = $("#videos")
 
    $("#form").submit(function(event) {
         event.preventDefault()
         alert('Form is submitted')
-
-
 
         var search = $("#search").val()
 
@@ -16,8 +17,12 @@ $(document).ready(function(){
    })
 
    function videoSearch(key,search,maxResults){
-        $.get("https://www.googleapis.com/youtube/v/search/key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data){
+        $.get("https://www.googleapis.com/youtube/v/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data){
           console.log(data)
+
+          data.items.forEach(items => {
+               
+          });
         })
    }
 
