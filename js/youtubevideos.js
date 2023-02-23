bb87b7a6aba4e881c3f2a764c292e07817deb073
Ahmad Rabiu <rabiu2993@gmail.com>
2023-02-22 23:58:25 -1200

    I added more functions

---
+++ js/youtubevideos.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+   var API_KEY = "AIzaSyDDBXvXZlraYJmPP4OKq8XLwZxYNynE-3I"
+
+   $("#form").submit(function(event) {
+        event.preventDefault()
+        alert('Form is submitted')
+
+
+
+        var search = $("#search").val()
+
+        videoSearch(API_KEY,search,12)
+
+   })
+
+   function videoSearch(key,search,maxResults){
+        $.get("https://www.googleapis.com/youtube/v/search/key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data){
+          console.log(data)
+        })
+   }
+
+})
